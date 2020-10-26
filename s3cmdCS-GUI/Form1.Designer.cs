7691b3f92016d611ffb67b14e34cf15d96653630
porya0ras <porya_ras@the4.ir>
2020-10-26 16:15:55 +0330

    ایجات پروژه GUI

--- s3cmdCS-GUI/Form1.Designer.cs
+++ s3cmdCS-GUI/Form1.Designer.cs
@@ -29,47 +29,154 @@ namespace s3cmdCS_GUI
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.radListView1 = new Telerik.WinControls.UI.RadListView();
-            ((System.ComponentModel.ISupportInitialize)(this.radListView1)).BeginInit();
+            this.BucketsList = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button4 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 458);
+            this.button1.Location = new System.Drawing.Point(12, 55);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(145, 34);
             this.button1.TabIndex = 2;
-            this.button1.Text = "Click Me!";
+            this.button1.Text = "Load!";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // radListView1
+            // BucketsList
             // 
-            this.radListView1.Location = new System.Drawing.Point(12, 12);
-            this.radListView1.Name = "radListView1";
-            this.radListView1.Size = new System.Drawing.Size(722, 441);
-            this.radListView1.TabIndex = 3;
-            this.radListView1.Text = "radListView1";
+            this.BucketsList.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.BucketsList.FormattingEnabled = true;
+            this.BucketsList.Location = new System.Drawing.Point(94, 21);
+            this.BucketsList.Name = "BucketsList";
+            this.BucketsList.Size = new System.Drawing.Size(184, 30);
+            this.BucketsList.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.progressBar1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.BucketsList);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox1.Location = new System.Drawing.Point(0, 492);
+            this.groupBox1.MaximumSize = new System.Drawing.Size(0, 100);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(584, 100);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Actions Box";
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Margin = new System.Windows.Forms.Padding(3, 3, 3, 150);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(584, 492);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.listView1_ColumnClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "BUCKETS ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(163, 55);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 34);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Upload File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(314, 55);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(173, 34);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Delete Selected Files";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(0, 96);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(584, 4);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(493, 55);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(73, 34);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Save";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(746, 503);
-            this.Controls.Add(this.radListView1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(584, 592);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.groupBox1);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximumSize = new System.Drawing.Size(602, 639);
+            this.MinimumSize = new System.Drawing.Size(602, 639);
             this.Name = "Form1";
             this.Text = "explorer";
-            ((System.ComponentModel.ISupportInitialize)(this.radListView1)).EndInit();
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
         private System.Windows.Forms.Button button1;
-        private Telerik.WinControls.UI.RadListView radListView1;
+        private System.Windows.Forms.ComboBox BucketsList;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
 
--- s3cmdCS-GUI/Form1.cs
+++ s3cmdCS-GUI/Form1.cs
@@ -1,12 +1,19 @@
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.S3.Transfer;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
+using Telerik.WinControls.UI;
 
 // This is the code for your desktop app.
 // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
@@ -15,21 +22,301 @@ namespace s3cmdCS_GUI
 {
     public partial class Form1 : Form
     {
+        string accessKey, secretKey, endpoint;
+        AmazonS3Client s3Client;
+        AmazonS3Config config = new AmazonS3Config();
+        private ListViewColumnSorter lvwColumnSorter;
+        Thread upload_t;
         public Form1()
         {
+
             InitializeComponent();
+
+            accessKey = getSetting("accessKey");
+            secretKey = getSetting("secretKey");
+            endpoint = getSetting("Endpoint");
+
+            config.ServiceURL = endpoint;
+
+            s3Client = new AmazonS3Client(
+                    accessKey,
+                    secretKey,
+                    config
+                    );
+
+            listView1.Bounds = new Rectangle(new Point(10, 10), new Size(300, 200));
+            // Set the view to show details.
+            listView1.View = View.Details;
+            // Allow the user to edit item text.
+            listView1.LabelEdit = false;
+            // Allow the user to rearrange columns.
+            listView1.AllowColumnReorder = true;
+            // Display check boxes.
+            listView1.CheckBoxes = true;
+            // Select the item and subitems when selection is made.
+            listView1.FullRowSelect = true;
+            // Display grid lines.
+            listView1.GridLines = true;
+            // Sort the items in the list in ascending order.
+            listView1.Sorting = SortOrder.Ascending;
+
+            // Create columns for the items and subitems.
+            // Width of -2 indicates auto-size.
+            lvwColumnSorter = new ListViewColumnSorter();
+            this.listView1.ListViewItemSorter = lvwColumnSorter;
         }
 
-        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            // Click on the link below to continue learning how to build a desktop app using WinForms!
-            System.Diagnostics.Process.Start("http://aka.ms/dotnet-get-started-desktop");
 
-        }
 
+
+
+
+        bool switch_connection = false;
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Thanks!");
+            ListObjectsV2Request request = new ListObjectsV2Request();
+            request.BucketName = BucketsList.SelectedValue.ToString();
+            ListObjectsV2Response response = s3Client.ListObjectsV2(request);
+            listView1.Clear();
+            foreach (var row in response.S3Objects)
+            {
+                // Create three items and three sets of subitems for each item.
+                ListViewItem item1 = new ListViewItem(row.Key.TrimEnd(), 0);
+                item1.SubItems.Add(row.LastModified.ToString("G"));
+                item1.SubItems.Add(row.Size.ToSize(MyExtension.SizeUnits.GB));
+                item1.SubItems.Add(row.ETag);
+                listView1.Items.Add(item1);
+            }
+            listView1.Columns.Add("File", 230, HorizontalAlignment.Left);
+            listView1.Columns.Add("LastModified", 140, HorizontalAlignment.Left);
+            listView1.Columns.Add("Size(GB)", -2, HorizontalAlignment.Left);
+            listView1.Columns.Add("ETag", -2, HorizontalAlignment.Center);
+
+        }
+
+        private void listView1_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            if (e.Column == lvwColumnSorter.SortColumn)
+            {
+                // Reverse the current sort direction for this column.
+                if (lvwColumnSorter.Order == SortOrder.Ascending)
+                {
+                    lvwColumnSorter.Order = SortOrder.Descending;
+                }
+                else
+                {
+                    lvwColumnSorter.Order = SortOrder.Ascending;
+                }
+            }
+            else
+            {
+                // Set the column number that is to be sorted; default to ascending.
+                lvwColumnSorter.SortColumn = e.Column;
+                lvwColumnSorter.Order = SortOrder.Ascending;
+            }
+
+            // Perform the sort with these new sort options.
+            this.listView1.Sort();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var keyName = Path.GetFileName(openFileDialog1.FileName);
+
+                    var fileTransferUtility = new TransferUtility(s3Client);
+
+                    // Use TransferUtilityUploadRequest to configure options.
+                    // In this example we subscribe to an event.
+                    var uploadRequest =
+                        new TransferUtilityUploadRequest
+                        {
+                            BucketName = BucketsList.SelectedValue.ToString(),
+                            FilePath = openFileDialog1.FileName,
+                            Key = keyName
+                        };
+
+                    uploadRequest.UploadProgressEvent += UploadRequest_UploadProgressEvent;
+
+                    upload_t = new Thread(delegate ()
+                    {
+                        try
+                        {
+                            fileTransferUtility.Upload(uploadRequest);
+                            MessageBox.Show("Upload completed!");
+                            //progressBar1.Value = 0;
+                        }
+                        catch (AmazonS3Exception amazonS3Exception)
+                        {
+                            if (amazonS3Exception.ErrorCode != null &&
+                                (amazonS3Exception.ErrorCode.Equals("InvalidAccessKeyId")
+                                ||
+                                amazonS3Exception.ErrorCode.Equals("InvalidSecurity")))
+                            {
+                                MessageBox.Show("Check the provided AWS Credentials.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                            else
+                            {
+                                MessageBox.Show("Error occurred: " + amazonS3Exception.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                        }
+                    });
+                    upload_t.Start();
+                }
+                catch (AmazonS3Exception amazonS3Exception)
+                {
+                    if (amazonS3Exception.ErrorCode != null &&
+                        (amazonS3Exception.ErrorCode.Equals("InvalidAccessKeyId")
+                        ||
+                        amazonS3Exception.ErrorCode.Equals("InvalidSecurity")))
+                    {
+                        MessageBox.Show("Check the provided AWS Credentials.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error occurred: " + amazonS3Exception.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+
+            }
+        }
+
+        private void UploadRequest_UploadProgressEvent(object sender, UploadProgressArgs e)
+        {
+            if (progressBar1.Parent.InvokeRequired)
+            {
+                progressBar1.Parent.Invoke(new MethodInvoker(delegate
+                {
+                    progressBar1.Maximum = 100;
+                    progressBar1.Value = Convert.ToInt32(((double)e.TransferredBytes / e.TotalBytes) * 100);
+                }));
+            }
+            else
+            {
+                progressBar1.Maximum = 100;
+                progressBar1.Value = Convert.ToInt32(((double)e.TransferredBytes / e.TotalBytes) * 100);
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (upload_t != null)
+                upload_t.Abort();
+            Application.Exit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are You Sure?", "Question", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                if (MessageBox.Show("Are You Realy Sure?", "Question", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    foreach (ListViewItem item in this.listView1.CheckedItems)
+                    {
+                        var tag = item.Text;
+                        // do some operation using tag
+                        DeleteObjectRequest request = new DeleteObjectRequest();
+                        request.BucketName = BucketsList.SelectedValue.ToString();
+                        request.Key = tag;
+                        var resp = s3Client.DeleteObject(request);
+                        button1_Click(null, null);
+                    }
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var fileTransferUtility = new TransferUtility(s3Client);
+            saveFileDialog1.FileName = this.listView1.CheckedItems[0].Text;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var downloadRequest =
+                      new TransferUtilityDownloadRequest
+                      {
+                          BucketName = BucketsList.SelectedValue.ToString(),
+                          FilePath = saveFileDialog1.FileName,
+                          Key = this.listView1.CheckedItems[0].Text
+                      };
+                    downloadRequest.WriteObjectProgressEvent += DownloadRequest_WriteObjectProgressEvent;
+                    upload_t = new Thread(delegate ()
+                    {
+                        try
+                        {
+                            fileTransferUtility.Download(downloadRequest);
+                            MessageBox.Show("Download completed!");
+                            //progressBar1.Value = 0;
+                        }
+                        catch (AmazonS3Exception amazonS3Exception)
+                        {
+                            if (amazonS3Exception.ErrorCode != null &&
+                                (amazonS3Exception.ErrorCode.Equals("InvalidAccessKeyId")
+                                ||
+                                amazonS3Exception.ErrorCode.Equals("InvalidSecurity")))
+                            {
+                                MessageBox.Show("Check the provided AWS Credentials.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                            else
+                            {
+                                MessageBox.Show("Error occurred: " + amazonS3Exception.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                        }
+                    });
+                    upload_t.Start();
+                }
+                catch (AmazonS3Exception amazonS3Exception)
+                {
+                    if (amazonS3Exception.ErrorCode != null &&
+                        (amazonS3Exception.ErrorCode.Equals("InvalidAccessKeyId")
+                        ||
+                        amazonS3Exception.ErrorCode.Equals("InvalidSecurity")))
+                    {
+                        MessageBox.Show("Check the provided AWS Credentials.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error occurred: " + amazonS3Exception.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void DownloadRequest_WriteObjectProgressEvent(object sender, WriteObjectProgressArgs e)
+        {
+            if (progressBar1.Parent.InvokeRequired)
+            {
+                progressBar1.Parent.Invoke(new MethodInvoker(delegate
+                {
+                    progressBar1.Maximum = 100;
+                    progressBar1.Value = Convert.ToInt32(((double)e.TransferredBytes / e.TotalBytes) * 100);
+                }));
+            }
+            else
+            {
+                progressBar1.Maximum = 100;
+                progressBar1.Value = Convert.ToInt32(((double)e.TransferredBytes / e.TotalBytes) * 100);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListBucketsResponse response = s3Client.ListBuckets();
+            BucketsList.DataSource = response.Buckets;
+            BucketsList.DisplayMember = "BucketName";
+            BucketsList.ValueMember = "BucketName";
+
+        }
+        public string getSetting(string fild)
+        {
+            XDocument doc = XDocument.Load(Application.StartupPath + @"\Settings.xml");
+            List<XElement> setting = doc.Root.Elements().Where(x => x.Name.LocalName == fild).ToList<XElement>();
+            return setting.First().Value;
         }
     }
 }
